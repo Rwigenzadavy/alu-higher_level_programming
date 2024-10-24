179f83bc4f8b42582883d35f472f4783752b3da0
Rwigenzadavy <r.niyoyande@alustudent.com>
2024-10-24 12:37:51 +0200

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for n in range(10):
-    for m in range(n + 1, 10):
- print("{}{}".format(n, m),
-    end=", "
-    if format("{}{}".format(n, m)) != "89" else "\n")
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}, ".format(i, j), end="")
