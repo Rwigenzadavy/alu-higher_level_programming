e1e1032f67e4bc56f81674542d0d12d38c22d506
Rwigenzadavy <r.niyoyande@alustudent.com>
2024-10-25 11:10:00 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    param_len = len(argv) - 1
-    label_str = 'argument' if param_len == 1 else 'arguments'
-    label_punct = '.' if param_len == 0 else ':'
-    print(f"{param_len} {label_str}{label_punct}")
-    for arg in argv[1:]:
-        print(f"{argv.index(arg)}: {arg}")
+from sys import argv
+x = len(argv) - 1
+if x < 1:
+    print("{} arguments.".format(x))
+elif x == 1:
+    print("{} argument:".format(x))
+else:
+    print("{} arguments:".format(x))
+for i in range(x):
+    print("{} : {:s}".format(i + 1, argv[i + 1]))
