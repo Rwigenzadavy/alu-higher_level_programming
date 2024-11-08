242ebcc65e05d86a977fe3b4f0ef265c525f7109
davy <r.niyoyande@alustudent.com>
2024-11-08 04:55:36 -0800

    1-square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Define a square class"""
 
+
 class Square:
     """square class"""
 
-    def __init__(self, size):
+    def __init__(self, size) -> None:
         """ initialization """
         self.__size = size
