9a6a780c8e6fe7fcbbfb03228ed64e943d148e76
davy <r.niyoyande@alustudent.com>
2024-11-08 03:39:01 -0800

    String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
     def __str__(self):
         """ the string representation of the object """
         if self.__height == 0 or self.__width == 0:
-            return ""        
+            return("")        
         rec_print = []
         for i in range(self.__height):
             [rec_print.append("#") for j in range(self.width)]
