8e8e31d83a6e66f90666fa05aae6e2fa6db1bfcb
davy <r.niyoyande@alustudent.com>
2024-11-16 12:06:05 -0800

    task done

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,9 @@
+--
+SELECT DISTINCT
+    s1.score,
+    (SELECT
+         count(*)
+     FROM second_table as s2
+     WHERE s2.score = s1.score) as number
+FROM second_table as s1
+ORDER BY number DESC;
